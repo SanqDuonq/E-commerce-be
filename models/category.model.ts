01057c4c8ca599c10ef484b54942f816ad124a7c
SanqDuonq <namsang0902s@gmail.com>
2025-01-08 22:53:37 +0700

    feat: category model

--- models/category.model.ts
+++ models/category.model.ts
@@ -1,1 +1,11 @@
+import mongoose from 'mongoose';
 
+const CategoryModel = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
+}, {collection: 'Category'})
+
+const Category = mongoose.model('Category',CategoryModel);
+export default Category;
