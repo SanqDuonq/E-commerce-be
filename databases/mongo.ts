b075e7e9ac55dd5ea05f2cf6a1356d60b482e483
SanqDuonq <namsang0902s@gmail.com>
2025-04-01 22:44:29 +0700

    feat: singleton

--- databases/mongo.ts
+++ databases/mongo.ts
@@ -1,27 +1,15 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
+import { configManager } from '../singleton/singleton.config';
 
-class Database {
-    private static instance: Database;
-
-    private constructor() {}
-
-    public static getInstance(): Database {
-        if (!Database.instance) {
-            Database.instance = new Database();
-        }
-        return Database.instance;
-    }
-
-    public async connectMongoDB() {
-        try {
-            const connect = await mongoose.connect(process.env.MongoURI!);
-            console.log(`Connected successful mongoDB ${connect.connection.host}`);
-        } catch (error) {
-            console.log(`Connected failed mongoDB ${error}`)
-        }
+const connectMongoDB = async () => {
+    try {
+        const connect = await mongoose.connect(configManager.get('MongoURI'));
+        console.log(`Connected successful mongoDB ${connect.connection.host}`);
+    } catch (error) {
+        console.log(`Connected failed mongoDB ${error}`)
     }
 }
 
-export default Database;
+export default connectMongoDB;
+
+
